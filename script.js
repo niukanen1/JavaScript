7618c723275a1e50907ba7eef7cba0e4edda6e35
ivanNiukanen <ivanniu2003@gmail.com>
2021-09-01 13:20:16 +0300

    as

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"; 
+const typeGoods = "food"; 
+
+
+console.log(`https://markett.com/${typeGoods}`)
+
+const arrFunc = () => { 
+    console.log("Hello")
+    console.log(Math.PI)    
+}
+arrFunc() 
